dafe39935921844331e4db455934fb8ec71f9c01
pygmalion <m@h>
2019-11-05 21:05:57 +0100

    further work

--- frontend/public/js/src/app.js
+++ frontend/public/js/src/app.js
@@ -1,3 +1,7 @@
-import { createElement, Component, render } from '../contrib/node_modules/preact/src/index.js'
-
-console.log("Hello Liver Server")
+import { h, Component, render } from '../contrib/node_modules/preact/src/index.js';
+class Test extends Component {
+    render(props, state) {
+        return (h("div", { id: "app" }, h("h1", {}, "Hallo Welt"), null));
+    }
+}
+render(h(Test, null), document.body);
