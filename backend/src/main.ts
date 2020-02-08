a8796364e96ef66d8872734078fb6e629078f0f5
pygmalion <m@h>
2020-02-08 10:52:43 +0100

    did some wor, like css bulma integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,6 +30,6 @@ const router = express.Router();
 
 //connectToMongo({server: mdbServer, port: mdbPport, dbName: mdb});
 //app.get('/', (req, res) => res.send('Hello World!'));
-
+app.use("/", userInfo(router));
 app.use("/", userInfo(router));
 app.listen(port, () => console.log(`Server started listening to port ${port}`));
--- frontend/public/js/src/app.ts
+++ frontend/public/js/src/app.ts
@@ -4,9 +4,65 @@ class Test extends Component {
     render(props, state) {
         return (
             h("div", {id:"app"},
-                h("h1",{}, "Hallo Welt"),
+                h("section", {class: "hero is-primary is-medium"},
+                    h("div",{class: "hero-head"}, 
+                        h("nav", {class:"navbar"},
+                            h("div", {class:"container"},
+                                h("div", {id:"navbarMenuHeroA", class: "navbar-menu"},
+                                    h("div", {class:"navbar-start"},
+                                        h("a", {class:"navbar-item"}, "Home"),
+                                        h("a", {class:"navbar-item"}, "Players"),
+                                        h("a", {class:"navbar-item"}, "Login")
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
             null)
         );
     }
 }
 render(h(Test,null), document.body);
+
+/*
+<section class="hero is-primary is-medium">
+  <!-- Hero head: will stick at the top -->
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroA" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">
+              Home
+            </a>
+            <a class="navbar-item">
+              Examples
+            </a>
+            <a class="navbar-item">
+              Documentation
+            </a>
+            <span class="navbar-item">
+              <a class="button is-primary is-inverted">
+                <span class="icon">
+                  <i class="fab fa-github"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  */
